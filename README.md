d595ddf17c2b12ac3c513408ca8cd033966aa83b
olivierrr <oliviersinbox@gmail.com>
2014-11-21 13:37:06 -0800

    readme

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ ziggy-cleverbot
 ===============
 
 cleverbot plugin for ziggy
+
+## `botnick` `<message>`
+
+`botnick` is your bots irc nickname by default, to override it pass a `botnick` property on the settings object from ziggy setup.
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = function(ziggy, settings) {
   var bot = new cleverBot()
 
   ziggy.on('message', function (user, channel, message) {
-    var reg = new RegExp(ziggy.settings.nickname)
+    var reg = new RegExp(settings.botnick || ziggy.settings.nickname)
 
     if(reg.test(message)) {
       var toWrite =  message.replace(reg, ' ').split(/\s+/).join(' ')

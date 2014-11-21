324c26d7f9d42562e98730a1634dd2c12abe6aff
olivierrr <oliviersinbox@gmail.com>
2014-11-21 13:02:33 -0800

    1.0

---
+++ index.js
@@ -0,0 +1,17 @@
+
+var cleverBot = require('cleverbot-node')
+
+module.exports = function(ziggy, settings) {
+  var bot = new cleverBot()
+
+  ziggy.on('message', function (user, channel, message) {
+    var reg = new RegExp(ziggy.settings.nickname)
+
+    if(reg.test(message)) {
+      var toWrite =  message.replace(reg, ' ').split(/\s+/).join(' ')
+      bot.write(toWrite,  function (resp) {
+        ziggy.say(channel, user.nick + ': ' resp.message)
+      })
+    }
+  })
+}
